a789f0d75181d533abc831a053a5eb3cd83bbd3d
annemusau <moigeanne646@gmail.com>
2024-08-13 21:15:29 +0800

    commiting all

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ function Hero() {
             <p className='sub-header'>Your Fast-Track to Property Deals: </p>
             <h1>Buy, Rent, Sell</h1>
             <p>Explore a wide range of properties, from luxurious apartments to cozy family homes, all tailored to your lifestyle and budget.
-            Whether you're buying your first home or searching for an investment property, our expert agents are here to guide you every step of the way.</p>
+            Whether youa're buying your first home or searching for an investment property, our expert agents are here to guide you every step of the way.</p>
         </div>
         <img src='house.png'/>
     </div>
--- src/components/PropertyDetail.js
+++ src/components/PropertyDetail.js
@@ -1,9 +1,51 @@
 import React from 'react'
 
-function PropertyDetail() {
+const PropertyDetails = ({ property, closeDetails }) => {
   return (
-    <div>PropertyDetail</div>
-  )
-}
+    <div>
+      {property ? (
+        <div className="property-details">
+          <h2>{property.title}</h2>
+          <div className="property-gallery">
+      
+            
+          </div>
+          <p>Location: {property.location}</p>
+          <p>Type: {property.propertyType}</p>
+          <p>{property.bedrooms} Bedrooms</p>
+          <p>Status: {property.status}</p>
+          <p>Description: {property.description}</p>
+          <p>Price: KSH {property.price.toLocaleString()}</p>
 
-export default PropertyDetail
+          <div className="specifications">
+            <h4>Specifications:</h4>
+            <p>Square Footage: {property.squareFootage} sq ft</p>
+            <p>Year Built: {property.yearBuilt}</p>
+            <p>Lot Size: {property.lotSize} acres</p>
+            <p>Garage: {property.garage ? 'Yes' : 'No'}</p>
+            <p>Swimming Pool: {property.pool ? 'Yes' : 'No'}</p>
+          </div>
+
+          <div className="amenities">
+            <h4>Amenities:</h4>
+            <ul>
+              {property.amenities && property.amenities.map((amenity, index) => (
+                <li key={index}>{amenity}</li>
+              ))}
+            </ul>
+          </div>
+          
+          <div className="contact-info">
+            <h4>Contact:</h4>
+            <p>Agent Name: {property.agentName}</p>
+            <p>Phone: {property.agentPhone}</p>
+            <p>Email: {property.agentEmail}</p>
+          </div>
+          <button onClick={closeDetails}>Close</button>
+        </div>
+      ) : (
+        <p>No property details available.</p>
+      )}
+    </div>
+  );
+};
