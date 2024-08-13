2ad951643c03f300bc645d79e0b8b6129b806cb1
Harmony <harmonymukolwe@gmail.com>
2024-08-13 17:57:15 +0300

    Adds styling to property card

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { IoLocationOutline, IoHeartCircleSharp } from "react-icons/io5";
+import { IoLocationOutline, IoHeartCircleOutline } from "react-icons/io5";
 import { LiaBedSolid } from "react-icons/lia";
 import { PiBathtub } from "react-icons/pi";
 import { TbRulerMeasure } from "react-icons/tb";
@@ -8,18 +8,23 @@ import "../styles/card.css";
 function PropertyCard() {
   return (
     <div className="property_card">
-      <img
-        src="https://img.freepik.com/free-photo/movie-night-by-pool-whole-family_1268-31098.jpg?t=st=1723550396~exp=1723553996~hmac=ddee877aa035c23140d9e28d68ab38dd2419c4a0d782447f761076ec2f60617c&w=826"
-        alt="property image"
-      />
-      <IoHeartCircleSharp />
-
-      <div className="property_card-content">
-        <div className="propertyy_card-tags">
+      <div className="property_card-img_container">
+        <img
+          className="property_card-img"
+          src="https://img.freepik.com/free-photo/movie-night-by-pool-whole-family_1268-31098.jpg?t=st=1723550396~exp=1723553996~hmac=ddee877aa035c23140d9e28d68ab38dd2419c4a0d782447f761076ec2f60617c&w=826"
+          alt="property image"
+        />
+        <IoHeartCircleOutline
+          className="like_icon icon"
+          style={{ color: "#ffffff" }}
+        />
+        <div className="property_card-tags">
           <small>For Rent</small>
           <small>Apartment</small>
         </div>
+      </div>
 
+      <div className="property_card-content">
         <div className="property_card-details">
           <div className="property_card-price">
             <h3>$6,000</h3>
@@ -29,7 +34,7 @@ function PropertyCard() {
           <p className="property_card-location">Riverview Retreat</p>
 
           <div className="property_card-address_container">
-            <IoLocationOutline />
+            <IoLocationOutline className="location_icon icon" />
             <p className="property_card-address">
               452 Robert Drives, Lake Maryhaven, NY 23531-2648
             </p>
@@ -37,15 +42,15 @@ function PropertyCard() {
 
           <div className="property_card-details_container">
             <div className="beds">
-              <LiaBedSolid />
+              <LiaBedSolid className="bed_icon icon" />
               <span>4 Beds</span>
             </div>
             <div className="bath">
-              <PiBathtub />
+              <PiBathtub className="bath_icon icon" />
               <span>2 Baths</span>
             </div>
-            <div className="bath">
-              <TbRulerMeasure />
+            <div className="plinth">
+              <TbRulerMeasure className="plinth_icon icon" />
               <span>3,000 sqft</span>
             </div>
           </div>
