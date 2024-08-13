e3aa576ffaaf4247c92c63619aae6591029bdc2e
Samson Njai <njaisamson@gmail.com>
2024-08-13 14:03:42 +0300

    Adds search logic in the SearchBar components.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchBar() {
+function SearchBar({ homehub }) {
+  const [search, setSearch] = useState("");
+
+  function handleSearchChange(event) {
+    event.preventDefault();
+    setSearch(event.target.value);
+  }
+
+  const filteredProperties = homehub.filter((property) => {
+    const toLowerCaseSearch = search.toLocaleLowerCase();
+    return (
+      property.title.toLowerCase().includes(toLowerCaseSearch) ||
+      property.location.toLowerCase().includes(toLowerCaseSearch) ||
+      property.propertyType.toLowerCase().includes(toLowerCaseSearch) ||
+      property.description.toLowerCase().includes(toLowerCaseSearch)
+    );
+  });
+
   return (
     <div className="search-form-container">
       <form className="search-form">
         <input
           type="text"
+          value={search}
+          onChange={handleSearchChange}
           placeholder="Search property here..."
           name="search"
           id="search"
         />
       </form>
+      <div className="search-results">
+        <ul>
+          {filteredProperties.map((property) => (
+            <li key={property.id}>
+              <h3>Title: {property.title}</h3>
+              <p>Location: {property.location}</p>
+              <p>Type: {property.propertyType}</p>
+              <p>Description: {property.description}</p>
+              <p>Price: {property.price}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
