77d0d2d6d6e9177fcec94eea5f12b1a20a2f5792
Harmony <harmonymukolwe@gmail.com>
2024-08-16 12:39:58 +0300

    Adds css transitions to app buttons

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { IoHeartCircleOutline } from "react-icons/io5";
 import { FaLocationDot } from "react-icons/fa6";
 import { LiaBedSolid } from "react-icons/lia";
@@ -7,16 +7,21 @@ import { PiBathtub } from "react-icons/pi";
 import { TbRulerMeasure } from "react-icons/tb";
 import "../styles/card.css";
 
-function PropertyCard({properties, postToWishlist, handleDelete, inWishlist, isLiked, setIsLiked}) {
-   
+function PropertyCard({
+  properties,
+  postToWishlist,
+  handleDelete,
+  inWishlist,
+  isLiked,
+  setIsLiked,
+}) {
   function handleLike(id) {
-    
-    if (isLiked) {       
+    if (isLiked) {
       handleDelete(id);
     } else {
       postToWishlist(id);
     }
-    setIsLiked(!isLiked);      
+    setIsLiked(!isLiked);
   }
 
   return (
@@ -28,11 +33,13 @@ function PropertyCard({properties, postToWishlist, handleDelete, inWishlist, isL
           alt={properties.title}
           height={300}
         />
-        {!inWishlist && <IoHeartCircleOutline
-          className={`like_icon icon ${isLiked ? 'isLiked' : ''}`}
-          style={{ color: "#ffffff" }}            
-          onClick={() => handleLike(properties.id)}        
-        />}
+        {!inWishlist && (
+          <IoHeartCircleOutline
+            className={`like_icon icon ${isLiked ? "isLiked" : ""}`}
+            style={{ color: "#ffffff" }}
+            onClick={() => handleLike(properties.id)}
+          />
+        )}
         <div className="property_card-tags">
           <small>{properties.status}</small>
           <small>{properties.propertyType}</small>
@@ -43,18 +50,16 @@ function PropertyCard({properties, postToWishlist, handleDelete, inWishlist, isL
         <div className="property_card-details">
           <h2 className="property_card-location">{properties.title}</h2>
           <div className="property_card-price">
-            <h3>Kes {properties.price.toLocaleString()}</h3> 
-            {properties.status === "For Rent"? <span>/year</span> : null }
-          </div>          
+            <h3>Kes {properties.price.toLocaleString()}</h3>
+            {properties.status === "For Rent" ? <span>/year</span> : null}
+          </div>
 
           <div className="property_card-address_container">
-            <FaLocationDot className="location_icon icon"/>
-            
-            <p className="property_card-address">
-              {properties.location}
-            </p>
+            <FaLocationDot className="location_icon icon" />
+
+            <p className="property_card-address">{properties.location}</p>
           </div>
-          <hr/>
+          <hr />
           <div className="property_card-details_container">
             <div className="beds">
               <LiaBedSolid className="bed_icon icon" />
@@ -69,7 +74,12 @@ function PropertyCard({properties, postToWishlist, handleDelete, inWishlist, isL
               <span>{properties.squareFootage} sqft</span>
             </div>
           </div>
-          <Link className="view-property-btn" to={`/properties-detail/${properties.id}`}>View Property</Link>
+          <Link
+            className="view-property-btn"
+            to={`/properties-detail/${properties.id}`}
+          >
+            View Property
+          </Link>
         </div>
       </div>
     </div>
